8d53bc10d1b9b6397eac0c1c740c8912bb1f510e
Gowtham-SF4769 <gowtham.ravi@syncfusion.com>
2026-04-24 12:24:57 +0530

    Incremental Loading sample added

---
+++ LoadOnDemand/LoadOnDemand/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LoadOnDemand
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LoadOnDemand/LoadOnDemand/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LoadOnDemand
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
