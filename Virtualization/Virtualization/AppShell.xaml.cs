1aa72777d0c5a12c2281c432a745002493600ae7
SethupathyD <sethupathy.d@syncfusion.com>
2026-04-24 12:05:00 +0530

    Code changes regarding Virtualization sample

---
+++ Virtualization/Virtualization/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Virtualization
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Virtualization/Virtualization/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Virtualization
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
